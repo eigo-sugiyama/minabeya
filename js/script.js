f9c4c54c31e3956585a7dec208c9e1ab0581feff
eigosugiyama <eigo.sugiyama0403@gmail.com>
2024-08-27 18:57:13 +0900

    script位置・headernav透過・バーガーオレンジ・fixedスクリプト

--- js/script.js
+++ js/script.js
@@ -1,3 +1,29 @@
+
+//sticky header
+
+$(function() {
+  var $win = $(window),
+      $fv = $('.fv'),
+      $header = $('.header')
+      fvHeight = $fv.outerHeight(),
+      fixedClass = 'fixed';
+
+$win.on('load scroll',function(){
+  var value = $(this).scrollTop();
+    if($win.width()>768)
+
+      if ( value > fvHeight) {
+        $header.addClass(fixedClass);
+        
+      } else {
+        $header.removeClass(fixedClass);
+        
+      }
+    
+
+  });
+});
+
   //ハンバーガーメニューの開閉
   $('.burger-btn').on('click',function(){
     $('.header-nav').fadeToggle(300);
